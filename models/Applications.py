8e7235657fc7f056cdd651ed21cf209a8cdfa16f
Pavel Filippenko <rubikpf2002@mail.ru>
2022-05-19 18:50:48 +0300

    Add some fields to structure computer

---
+++ models/Applications.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import relationship
+from models.Database import Base
+
+# вспомогательная таблица, которая используется для определения связей "Многие ко многим"
+assotiated_table = Table('assotioation', Base.metadata,
+                                         Column('application_id', Integer, ForeignKey('applications.id')),
+                                         Column('computer_id', Integer, ForeignKey('computers.id'))
+                                         )
+
+class Application(Base):
+    __tablename__ = 'applications'
+
+    id = Column(Integer, primary_key=True)
+    app_name = Column(String)
+
+    computers = relationship('Computer', secondary=assotiated_table, backref='group_lesson')
+
+    def __init__(self, app_name: str):
+        self.app_name = app_name
+
+    def __repr__(self) -> str:
+        return f'Application [ID: {self.id}, Name: {self.app_name}]'
--- models/Computer.py
+++ models/Computer.py
@@ -5,27 +5,41 @@ from models.Database import Base
 class Computer(Base):
     __tablename__ = 'computers'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer)
     user = relationship('User')
 
     first_window = Column(String)
     second_window = Column(String)
     third_window = Column(String)
-
     first_window_percent = Column(Float)
     second_window_percent = Column(Float)
     third_window_percent = Column(Float)
 
+    proc_number = Column(Integer)
+    disk_mem_usege = Column(Float)
+    CPU_f_min = Column(Float)
+    CPU_f_max = Column(Float)
+    CPU_f_cur = Column(Float)
+    Boot_time = Column(String)
+    Total_mem_used = Column(Float)
+
     date = Column(DateTime)
     time = Column(Time)
 
-    # по умолчанию
-    # def __init__(self):
+    def __init__(self, id: int):
+        self.id = id
     
     def __repr__(self) -> str:
         info = f'COMPUTER\n \
                     [ID: {self.id}]\n \
                     [FIRST WINDOW: {self.first_window} ({self.first_window_percent} %)]\n \
                     [SECOND WINDOW: {self.second_window} ({self.second_window_percent} %)]\n \
-                    [THIRD WINDOW: {self.third_window} ({self.third_window_percent} %)]\n \
+                    [THIRD WINDOW: {self.third_window} ({self.third_window_percent} %)]\n\n \
+                    [PROCESS NUMBER: {self.proc_number}]\n \
+                    [DISK MEMORY USAGE: {self.disk_mem_usege}]\n \
+                    [CPU FREUENCY (MIN): {self.CPU_f_min}]\n \
+                    [CPU FREUENCY (MAX): {self.CPU_f_max}]\n \
+                    [CPU FREUENCY (CURRENT): {self.CPU_f_cur}]\n \
+                    [BOOT TIME: {self.Boot_time}]\n \
+                    [TOTAL MEMORY USED: {self.Total_mem_used}]\n \
                     [DATE: ({self.date}) {self.time}]'
